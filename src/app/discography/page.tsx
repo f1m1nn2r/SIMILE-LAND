3101bc8831aeb3ebcb6c612b79814c656416bf0e
김민중 <f1minn2r@naver.com>
2026-05-11 15:27:03 +0900

    feat: 디스코그래피 리스트 페이지 퍼블리싱

--- src/app/discography/page.tsx
+++ src/app/discography/page.tsx
@@ -1,3 +1,54 @@
-export default function Discography() {
-  return <div>디스코그래피 페이지</div>;
+import Image from "next/image";
+import style from "./discography-list.module.scss";
+import { Happy } from "@/src/components/common/happy";
+import { DiscographyCard } from "@/src/features/discography";
+import { discographyData } from "@/src/features/discography/constants";
+import { Typography } from "@/src/components/common/typography";
+
+export default function DiscographyList() {
+  return (
+    <div className="discography-list">
+      <div className={style["discography-list__hero"]}>
+        <Typography
+          size="display"
+          weight="bold"
+          className={style["discography-list__title"]}
+        >
+          <span>DISCO</span>
+          <span className={style["discography-list__title-line2"]}>GRAPHY</span>
+        </Typography>
+        <div className={style["discography-list__deco1"]}>
+          <Image
+            src="/assets/images/discography-deco1.png"
+            alt=""
+            width={360}
+            height={450}
+            priority
+          />
+        </div>
+        <div className={style["discography-list__deco2"]}>
+          <Image
+            src="/assets/images/discography-deco2.png"
+            alt=""
+            width={250}
+            height={380}
+            priority
+          />
+        </div>
+        <div className={style["discography-list__happy"]}>
+          <Happy />
+        </div>
+      </div>
+
+      <div className={style["discography-list__grid"]}>
+        {discographyData.map((album) => (
+          <DiscographyCard
+            key={album.id}
+            {...album}
+            href={`/discography/${album.id}`}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
